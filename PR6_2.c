a332f8e7bb08dfc11b81b980f4e25584ede6088c
Kunal Gangani <150250846+kunal-gangani@users.noreply.github.com>
2023-12-26 14:09:46 +0530

    Committed

--- PR6_2.c
+++ PR6_2.c
@@ -1,26 +1,38 @@
-#include <stdio.h>
-#include <string.h>
-void main(){
-    char alpha[100];
-    int i,len;
-    int frequency[26];
-	printf("Enter any string: ");
-    gets(alpha);
-	len = strlen(alpha);
-	for(i=0; i<26; i++){
-        frequency[i] = 0;
-    }
-	for(i=0; i<len; i++){
-        if(alpha[i]>='a' && alpha[i]<='z'){
-            frequency[alpha[i] - 97]++;
-        }else if(alpha[i]>='A' && alpha[i]<='Z'){
-            frequency[alpha[i] - 65]++;
-        }
-    }
-	printf("\nFrequency of all characters in the given string: \n");
-    for(i=0; i<26; i++){
-        if(frequency[i] != 0){
-            printf("'%c' = %d\n", (i + 97), frequency[i]);
-        }
-    }
+/*Q.2 Write a Program to count the frequency of each character in a given string.
+Input:
+Enter any string: development
+Output:
+Frequency of each letter:
+d => 1
+e => 2
+v => 1
+l => 1
+o => 1
+p => 1
+m => 1
+n => 1
+/t => 1
+*/
+
+#include<stdio.h>
+ main(){
+	char str[100];
+	int i,j,count,num;
+	printf("Enter any String :");
+	scanf("%s",&str);
+	for(i=0; str[i]; i++);
+	num=i;
+	for(j=0; j<num; j++){
+		count=1;
+		if(str[j]){
+			for(i=j+1; i<num; i++){
+				if(str[j]==str[i]){
+					count++;
+					str[i]=NULL;
+				}
+			}
+		 printf(" '%c' = %d \n",str[j],count);
+		}
+		
+	}
 }
